06040d93a08b668c2863815193abe3d37f5e6d76
thiagobrazil <thiagobrazil007@gmail.com>
2025-10-08 13:50:05 -0300

    Update entrega_1.R

--- rdocs/entrega_1.R
+++ rdocs/entrega_1.R
@@ -22,3 +22,43 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+relatorio_vendas<- read_xlsx("relatorio_old_town_road.xlsx", sheet= "relatorio_vendas")
+
+info_vendas<- read_xlsx("relatorio_old_town_road.xlsx", sheet= "infos_vendas")|>
+  rename(SaleID = Sal3ID)
+
+info_produtos<- read_xlsx("relatorio_old_town_road.xlsx", sheet= "infos_produtos")|>
+  rename(ItemID = Ite3ID)
+
+info_lojas<- read_xlsx("relatorio_old_town_road.xlsx", sheet= "infos_lojas")|>
+  rename(StoreID = Stor3ID)
+
+vendas_produtos<- inner_join(info_vendas, info_produtos, by = "ItemID")
+
+vendas<- inner_join(relatorio_vendas, vendas_produtos, by = "SaleID")
+
+old_town_road<- inner_join(vendas, info_lojas, by = "StoreID") 
+
+old_town_road<- old_town_road|>
+  mutate(Date = year(Date))|>
+  rename(Year = Date)|>
+  mutate(TotalPrice = 5.31  * UnityPrice* Quantity)|>
+  select(SaleID, Year, NameStore, TotalPrice)|>
+  arrange(Year)
+
+media_por_ano<- old_town_road|>
+  group_by(Year)|>
+  summarise(StoreMean = sum(TotalPrice)/ 18)
+
+Gráfico<- ggplot(media_por_ano) +
+  aes(x=Year, y=StoreMean, group=1) +
+  geom_line(size=1,colour="#A11D21") + geom_point(colour="#A11D21",
+                                                  size=2) +
+  labs(x="Ano", y="Receita total média") +
+  theme_estat()
+ggsave("series_uni.pdf", width = 158, height = 93, units = "mm")
+
+Gráfico<- Gráfico +
+  scale_x_continuous(breaks = c(1880, 1881, 1882, 1883, 1884,1885, 1886, 1887, 1888, 1889))
+
