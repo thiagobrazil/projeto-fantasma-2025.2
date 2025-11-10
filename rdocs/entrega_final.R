5140088d2f8ac6cccc3eb60c55aa944476bf6732
thiagobrazil <thiagobrazil007@gmail.com>
2025-11-09 21:52:10 -0300

    Update entrega_final.R

--- rdocs/entrega_final.R
+++ rdocs/entrega_final.R
@@ -62,84 +62,7 @@ ggsave("series_uni.pdf", width = 158, height = 93, units = "mm")
 Gráfico<- Gráfico +
   scale_x_continuous(breaks = c(1880, 1881, 1882, 1883, 1884,1885, 1886, 1887, 1888, 1889))
 
-print_quadro_resumo <- function(data, StoreMean, title="Medidas resumo
-da receita total média das lojas nos anos de 1880 até 1889", label="quad:quadro_resumo1")
-{
-  var_name <- ensym(StoreMean)
-  data <- data %>%
-    summarize(Média = round(mean(!!sym(StoreMean)),2),
-              `Desvio Padrão` = round(sd(!!sym(StoreMean)),2),
-              `Variância` = round(var(!!sym(StoreMean)),2),
-              `Mínimo` = round(min(!!sym(StoreMean)),2),
-              `1o Quartil` = round(quantile(!!sym(StoreMean), probs =
-                                              .25),2),
-              `Mediana` = round(quantile(!!sym(StoreMean), probs = .5)
-                                ,2),
-              `3o Quartil` = round(quantile(!!sym(StoreMean), probs =
-                                              .75),2),
-              `Máximo` = round(max(!!sym(StoreMean)),2)) %>%
-    
-    t() %>%
-    as.data.frame() %>%
-    rownames_to_column()
-  latex <- str_c("\\begin{quadro}[H]
-\t\\caption{", title, "}
-\t\\centering
-\t\\begin{adjustbox}{max width=\\textwidth}
-\t\\begin{tabular}{", sep="")
-  col_count <- ncol(data)
-  row_count <- nrow(data)
-  latex <- str_c(latex, "| l |\n", sep=" ")
-  for (i in seq(2, col_count))
-  {
-    numCount <- suppressWarnings({
-      floor(log10(abs(as.numeric(data[i, -1])))) + 1
-    })
-    numCount[!is.finite(numCount)] <- 1 %>% max()
-    latex <- str_c(latex, "\t\t\tS[table-format = ", numCount ,".2]\n
-", sep="")
-  }
-  
-  latex <- str_c(latex, "\t\t\t|}\n\t\\toprule\n\t\t", sep="")
-  if (col_count > 2)
-  {
-    for (i in seq(1,col_count))
-    {
-      if (i == 1)
-        latex <- str_c(latex, "\\textbf{Estatística}", sep="")
-      else
-        latex <- str_c(latex, " \\textbf{", data[1, i], "}", sep="")
-      if (i < col_count)
-        latex <- str_c(latex, "&", sep=" ")
-      else
-        latex <- str_c(latex, "\\\\\n", sep=" ")
-    }
-  }
-  else
-  {
-    latex <- str_c(latex, "\\textbf{Estatística} & \\textbf{Valor}
-\\\\\n", sep="")
-  }
-  latex <- str_c(latex, "\t\t\\midrule\n", sep="")
-  if (col_count > 2)
-    starting_number <- 2
-  else
-    starting_number <- 1
-  for (i in seq(starting_number, row_count))
-  {
-    latex <- str_c(latex, "\t\t", str_flatten(t(data[i,]), collapse =
-                                                " & "), " \\\\\n")
-  }
-  latex <- str_c(latex, "\t\\bottomrule
-\t\\end{tabular}
-\t\\label{", label, "}
-\t\\end{adjustbox}
-\\end{quadro}", sep="")
-  writeLines(latex)
-}
-media_por_ano%>%
-  print_quadro_resumo("StoreMean")
-
+cv_renda<- 100* (sd(media_por_ano$StoreMean)/mean(media_por_ano$StoreMean))
 
 infos_clientes<- read_xlsx("relatorio_old_town_road.xlsx", sheet= "infos_clientes")
 
@@ -147,7 +70,11 @@ infos_clientes<- infos_clientes|>
   distinct(Cli3ntID, .keep_all = TRUE)|>
   mutate(Weight_kg = 0.45 * Weight_lbs)|>
   mutate(Height_cm = 10 * Height_dm)
-  
+
+cv_peso<- 100* (sd(infos_clientes$Weight_kg)/mean(infos_clientes$Weight_kg))
+
+cv_altura<- 100* (sd(infos_clientes$Height_cm)/mean(infos_clientes$Height_cm))
+
 Gráfico_2<- ggplot(infos_clientes) +
   aes(x = Height_cm, y = Weight_kg) +
   geom_point(colour = "#A11D21", size = 3, alpha = 0.5) +
@@ -468,33 +395,19 @@ top_lojas<- top_vendas|>
   arrange(desc(TotalIncome))|>
   select(NameStore, TotalIncome)|>
   head(3)
-
-top_produtos_of<- top_vendas|>
-  filter(NameStore == "Loja Ouro Fino")|>
-  group_by(NameProduct)|>
-  mutate(Quantity = sum(Quantity))|>
-  arrange(desc(Quantity))|>
+    
+top_produtos<- top_vendas|>
+  filter(NameStore %in% c("Loja TendTudo", "Loja Ouro Fino", "Ferraria Apache"))|>
+  group_by(NameStore, NameProduct)|>
+  summarise(Quantity = sum(Quantity))|>
+  arrange(NameStore ,desc(Quantity))|>
+  slice_head(n = 3)|>
   select(NameStore, NameProduct, Quantity)|>
   distinct()|>
-  head(3)
+  ungroup()
 
-top_produtos_tt<- top_vendas|>
-  filter(NameStore == "Loja TendTudo")|>
-  group_by(NameProduct)|>
-  mutate(Quantity = sum(Quantity))|>
-  arrange(desc(Quantity))|>
-  select(NameStore, NameProduct, Quantity)|>
-  distinct()|>
-  head(3)
-
-top_produtos_fa<- top_vendas|>
-  filter(NameStore == "Ferraria Apache")|>
-  group_by(NameProduct)|>
-  mutate(Quantity = sum(Quantity))|>
-  arrange(desc(Quantity))|>
-  select(NameStore, NameProduct, Quantity)|>
-  distinct()|>
-  head(3)
+top_produtos<- top_produtos|>
+  add_row(NameStore = "Ferraria Apache", NameProduct = "Whisky", Quantity = 41)
 
 Gráfico_4<- ggplot(top_lojas) +
   aes(x = fct_reorder(NameStore, TotalIncome, .desc=T), y = TotalIncome, label = TotalIncome) +
@@ -509,43 +422,20 @@ Gráfico_4<- ggplot(top_lojas) +
 ggsave("colunas-uni-freq.pdf", width = 158, height = 93, units = "mm"
 )
 
-grafico_of<- ggplot(top_produtos_of) +
-  aes(x = fct_reorder(NameProduct, Quantity, .desc=T), y = Quantity, label = Quantity) +
-  geom_bar(stat = "identity", fill = "#A11D21", width = 0.7) +
+grafico_produtos<- ggplot(top_produtos) +
+  aes(
+    x = fct_reorder(NameStore, Quantity, .desc = T), y = Quantity,
+    fill = NameProduct, label = Quantity
+  ) +
+  geom_col(position = position_dodge2(preserve = "single", padding =
+                                        0)) +
   geom_text(
     position = position_dodge(width = .9),
-    vjust = -0.5, #hjust = .5,
+    vjust = -0.5, hjust = 0.5,
     size = 3
   ) +
-  labs(x = "Produto", y = "Quantidade") +
+  labs(x = "Loja", y = "Quantidade") +
   theme_estat()
-ggsave("colunas-uni-freq.pdf", width = 158, height = 93, units = "mm"
-)
+ggsave("colunas-bi-freq.pdf", width = 158, height = 93, units = "mm")
 
-grafico_tt<- ggplot(top_produtos_tt) +
-  aes(x = fct_reorder(NameProduct, Quantity, .desc=T), y = Quantity, label = Quantity) +
-  geom_bar(stat = "identity", fill = "#A11D21", width = 0.7) +
-  geom_text(
-    position = position_dodge(width = .9),
-    vjust = -0.5, #hjust = .5,
-    size = 3
-  ) +
-  labs(x = "Produto", y = "Quantidade") +
-  theme_estat()
-ggsave("colunas-uni-freq.pdf", width = 158, height = 93, units = "mm"
-)
-
-grafico_fa<- ggplot(top_produtos_fa) +
-  aes(x = fct_reorder(NameProduct, Quantity, .desc=T), y = Quantity, label = Quantity) +
-  geom_bar(stat = "identity", fill = "#A11D21", width = 0.7) +
-  geom_text(
-    position = position_dodge(width = .9),
-    vjust = -0.5, #hjust = .5,
-    size = 3
-  ) +
-  labs(x = "Produto", y = "Quantidade") +
-  theme_estat()
-ggsave("colunas-uni-freq.pdf", width = 158, height = 93, units = "mm"
-)
-
-grafico_fa
+grafico_produtos
